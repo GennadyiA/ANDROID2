a0a8d3674dcd73c7be3972fce800b5ac22f83936
gennadyi <gennadyi90@gmail.com>
2022-07-14 13:40:59 +0300

    урок 3: доработка

--- app/src/main/java/com/example/android2/view/details/DetailsFragment.kt
+++ app/src/main/java/com/example/android2/view/details/DetailsFragment.kt
@@ -14,6 +14,7 @@ import com.example.android2.databinding.FragmentDetailsBinding
 import com.example.android2.model.Weather
 import com.example.android2.view.main.MainFragmentAdapter
 import com.example.android2.viewmodel.MainViewModel
+import com.example.android2.view.main.MainFragment
 
 
 class DetailsFragment : Fragment() {
@@ -32,15 +33,20 @@ class DetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val weather = arguments?.getParcelable<Weather>(BUNDLE_EXTRA)
         if (weather != null) {
-            val city = weather.city
-            binding.cityName.text = city.city
-            binding.cityCoordinates.text = String.format(
+            renderData(weather)
+        }
+    }
+    private fun renderData(weather: Weather) {
+        with(binding) {
+            cityName.text = weather.city.city
+            cityCoordinates.text = String.format(
                 getString(R.string.city_coordinates),
-                city.lat.toString(),
-                city.lon.toString()
+                weather.city.lat.toString(),
+                weather.city.lon.toString()
             )
-            binding.temperatureValue.text = weather.temperature.toString()
-            binding.feelsLikeValue.text = weather.feelsLike.toString()
+            temperatureValue.text = weather.temperature.toString()
+            feelsLikeValue.text = weather.feelsLike.toString()
+
         }
     }
     companion object {
